4a48fd29c1b165a71e79475c2d1d690283e6cc98
oliviernchima <nchima2010@gmail>
2018-04-24 09:38:30 +0200

    forum

---
+++ front/views/pages/discussion.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title> forum </title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!--BOOTSRAP CDN  -->
+    <!-- Latest compiled and minified CSS -->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+   <!-- jQuery library -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+   <!-- Popper JS -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+
+   <!-- Latest compiled JavaScript -->
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+<!--FIN BOOTSRAP CDN  -->
+
+<!--FONT AWESOME CDN -->
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+
+<!-- GOOGLE FONTS -->
+ <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+
+<!-- CSS  -->
+<link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+
+</head>
+<body>
+
+  <div class="container-fluid">
+    <!--icon de retour à la page forum + titre liste des forums -->
+  <div class="row">
+
+      <div class="col-md-1 col-1 espaceMenu text-center">
+          <p> <a href="forum.php"> <i class="fas fa-bars" style="color:#5eb9cb; margin-right:3px;"></i> </a> </p>
+          <p> <a href="discussion.php"> <i class="fas fa-edit " style="color:#5eb9cb" ></i> </a> </p>
+          <p> <a href="topic.php"> <i class="fas fa-eye " style="color:#5eb9cb"></i></i> </a> </p>
+      </div>
+
+      <div class="col-md-11 col-11 espaceContenu">
+
+          <div class="container discussionSpace">
+              <div class="row">
+                <div class="col-md-12 col-12 espace_discussion">
+                  <div class="discussion">
+                    <!-- contenu -->
+                    <div> Exercices du cours Bootstrap </div>
+                    <div> Par Boubou 4-Decembre-2018 à 22:00:00 </div>
+                  </div>
+
+                  <div class="compteurdemessage text-center">
+                    <!-- compteur de messages-->
+                    <div> 1326 messages </div>
+                  </div>
+
+                  <div class="infosDernierMsg text-center">
+                    <!--Infos dernier message-->
+                    <div>  Dernier message par Babou  </div>
+                    <div> 10 Fevrier 2019  à 22:00:00 </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12 col-12 espace_discussion">
+                <div class="discussion">
+                  <!-- contenu -->
+                  <div> Exercices du cours Bootstrap </div>
+                  <div> Par Boubou 4-Decembre-2018 à 22:00:00 </div>
+                </div>
+
+                <div class="compteurdemessage text-center">
+                  <!-- compteur de messages-->
+                  <div> 1326 messages </div>
+                </div>
+
+                <div class="infosDernierMsg text-center">
+                  <!--Infos dernier message-->
+                  <div>  Dernier message par Babou  </div>
+                  <div> 10 Fevrier 2019  à 22:00:00 </div>
+                </div>
+              </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 col-12 espace_discussion">
+              <div class="discussion">
+                <!-- contenu -->
+                <div> Exercices du cours Bootstrap </div>
+                <div> Par Boubou 4-Decembre-2018 à 22:00:00 </div>
+              </div>
+
+              <div class="compteurdemessage text-center">
+                <!-- compteur de messages-->
+                <div> 1326 messages </div>
+              </div>
+
+              <div class="infosDernierMsg text-center">
+                <!--Infos dernier message-->
+                <div>  Dernier message par Babou  </div>
+                <div> 10 Fevrier 2019  à 22:00:00 </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 col-12 espace_discussion">
+              <div class="discussion">
+                <!-- contenu -->
+                <div> Exercices du cours Bootstrap </div>
+                <div> Par Boubou 4-Decembre-2018 à 22:00:00 </div>
+              </div>
+
+              <div class="compteurdemessage text-center">
+                <!-- compteur de messages-->
+                <div> 1326 messages </div>
+              </div>
+
+              <div class="infosDernierMsg text-center">
+                <!--Infos dernier message-->
+                <div>  Dernier message par Babou  </div>
+                <div> 10 Fevrier 2019  à 22:00:00 </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12 col-12 espace_discussion">
+              <div class="discussion">
+                <!-- contenu -->
+                <div> Exercices du cours Bootstrap </div>
+                <div> Par Boubou 4-Decembre-2018 à 22:00:00 </div>
+              </div>
+
+              <div class="compteurdemessage text-center">
+                <!-- compteur de messages-->
+                <div> 1326 messages </div>
+              </div>
+
+              <div class="infosDernierMsg text-center">
+                <!--Infos dernier message-->
+                <div>  Dernier message par Babou  </div>
+                <div> 10 Fevrier 2019  à 22:00:00 </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12 col-12 espace_discussion">
+            <div class="discussion">
+              <!-- contenu -->
+              <div> Exercices du cours Bootstrap </div>
+              <div> Par Boubou 4-Decembre-2018 à 22:00:00 </div>
+            </div>
+
+            <div class="compteurdemessage text-center">
+              <!-- compteur de messages-->
+              <div> 1326 messages </div>
+            </div>
+
+            <div class="infosDernierMsg text-center">
+              <!--Infos dernier message-->
+              <div>  Dernier message par Babou  </div>
+              <div> 10 Fevrier 2019  à 22:00:00 </div>
+            </div>
+          </div>
+       </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ front/views/pages/topic.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title> forum </title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!--BOOTSRAP CDN  -->
+    <!-- Latest compiled and minified CSS -->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+   <!-- jQuery library -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+   <!-- Popper JS -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+
+   <!-- Latest compiled JavaScript -->
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+<!--FIN BOOTSRAP CDN  -->
+
+<!--FONT AWESOME CDN -->
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+
+<!-- GOOGLE FONTS -->
+ <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+
+<!-- CSS  -->
+<link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+
+</head>
+<body>
+
+<div class="container-fluid">
+<!--icon de retour à la page forum + titre liste des forums -->
+  <div class="row">
+
+    <div class="col-md-1 col-1 espaceMenu text-center">
+        <p> <a href="forum.php"> <i class="fas fa-bars" style="color:#5eb9cb"></i> </a> </p>
+        <p> <a href="discussion.php"> <i class="fas fa-edit " style="color:#5eb9cb" ></i> </a> </p>
+        <p> <a href="topic.php"> <i class="fas fa-eye " style="color:#5eb9cb"></i></i> </a> </p>
+    </div>
+
+    <div class="col-md-11 col-11 espaceContenu">
+      <div>
+        <h2> Liste des forums </h2>
+      </div>
+
+
+<div class="container">
+        <div class="row">
+          <h3> Site web </h3>
+          <div class="col-md-12 col-12  espace_siteweb">
+            <div class="contenu">
+              <!-- <p> contenu  1 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu   2 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu   3 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu   4 </p> -->
+            </div>
+          </div>
+        </div>
+  </div>
+
+
+  <div class="container">
+          <h3> Programmation </h3>
+        <div class="row">
+          <div class="col-md-12 col-12 espace_programmation">
+            <div class="contenu">
+              <!-- <p> contenu  1 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu  2 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu  3 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu  4 </p> -->
+            </div>
+
+            <div class="contenu">
+              <!-- <p> contenu  5 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu 6 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu  7 </p> -->
+            </div>
+            <div class="contenu">
+              <!-- <p> contenu  8 </p> -->
+            </div>
+        </div>
+      </div>
+
+    </div>
+
+    </div>
+
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
